ba5e786bd961348229b3e74167022c78f8e1bea8
AlbertoPereto <alberto.pereto@unisoma.com>
2021-07-16 18:08:34 +0000

    inclusão de extractor de CSV e ajustes no extrator JDBC

---
+++ R/extractor.R
@@ -0,0 +1,125 @@
+
+load_meta_data <- function(entity_catalog) {
+  library(DBI)
+  library(rJava)
+  library(RJDBC)
+
+  dbx_driver <- Sys.getenv("DATA_BOX_JDBC_DRIVER")
+  dbx_driver_path <- Sys.getenv("DATA_BOX_JDBC_LIB_PATH")
+  dbx_driver_host <- Sys.getenv("DATA_BOX_JDBC_HOST")
+  dbx_driver_user <- Sys.getenv("DATA_BOX_JDBC_USER")
+  dbx_driver_pwd <- Sys.getenv("DATA_BOX_JDBC_PWD")
+
+  if (dbx_driver=="") {
+    dbx_driver = "org.sqlite.JDBC"
+    dbx_driver_path = "/home/sqlite-jdbc-3.34.0.jar"
+    dbx_driver_host = "jdbc:sqlite:/home/data_box.db"
+    dbx_driver_user = "unisoma"
+    dbx_driver_pwd = ""
+  }
+  drv <- RJDBC::JDBC(dbx_driver, dbx_driver_path)
+  conn <- RJDBC::dbConnect(drv, dbx_driver_host, dbx_driver_user, dbx_driver_pwd)
+
+  # obtem os metadados para acesso ao dado requisitado
+  sql_args <- "select ea.arg_name, ea.arg_value, s.name as source
+               from dbx_source_engine_arg ea
+        inner join dbx_source_engine se on (ea.source_engine_id = se.source_id)
+                       inner join dbx_catalog c on (c.source_engine_id = se.id)
+                               inner join dbx_source s on (s.id = se.source_id)
+              where c.entity=?"
+  eargs <- dbGetQuery(conn, sql_args, entity_catalog)
+
+  # obtem os metadados para acesso ao dado requisitado
+  sql_defin <- "select cc.id, cc.name, cc.col_type, cc.is_not_null
+                from dbx_catalog c
+                inner join dbx_catalog_column cc on (cc.catalog_id = c.id)
+                where c.entity=? and cc.is_active=1"
+  edefn <- dbGetQuery(conn, sql_defin, entity_catalog)
+
+  # retorna dois objetos: (a) os argumentos para se conectar na fonte dos dados
+  # e (b) a definicao dos metadados da entidade.
+  return(list(entity_args = eargs, entity_definition = edefn))
+
+}
+
+load_jdbc_data <- function(jdbc_args, entity_metadata, entity_catalog){
+  library(stringr)
+
+  jdbc_driver <- ""
+  jdbc_host <- ""
+  jdbc_user <- ""
+  jdbc_pwd <- ""
+  jdbc_driver_path <- ""
+  jdbc_sql <- ""
+
+  for (i in 1:nrow(jdbc_args)) {
+    arg_name <- jdbc_args[i,1]
+    arg_value <- jdbc_args[i,2]
+
+    if (arg_name=="JDBC_DRIVER") {
+      jdbc_driver <- arg_value
+
+      if (str_detect(jdbc_driver, "sqlite")) {
+        jdbc_driver_path = "/home/sqlite-jdbc-3.34.0.jar"
+      } else if (str_detect(jdbc_driver, "postgresql")){
+        jdbc_driver_path = "/home/postgresql.jar"
+      }
+
+    } else if (arg_name=="JDBC_HOST") {
+      jdbc_host <- arg_value
+    } else if (arg_name=="JDBC_PWD") {
+      jdbc_pwd <- arg_value
+    } else if (arg_name=="JDBC_USER") {
+      jdbc_user <- arg_value
+    } else if (arg_name=="JDBC_SQL") {
+      jdbc_sql <- arg_value
+    }
+  }
+
+  drv <- RJDBC::JDBC(jdbc_driver, jdbc_driver_path)
+  conn <- RJDBC::dbConnect(drv, jdbc_host, jdbc_user, jdbc_pwd)
+
+  # roda a consulta
+  fields <- extract_fields(entity_metadata)
+  result_set <- dbGetQuery(conn, paste("select", fields, "from", jdbc_sql, sep=" "))
+
+  return(result_set)
+}
+
+load_csv_url_data <- function(csv_args, entity_metadata, entity_catalog){
+
+  csv_url <- ""
+  csv_sep <- ""
+  csv_dec_sep <- ""
+
+  for (i in 1:nrow(csv_args)) {
+    arg_name <- csv_args[i,1]
+    arg_value <- csv_args[i,2]
+
+    if (arg_name=="CSV_URL") {
+      csv_url <- arg_value
+    } else if (arg_name=="CSV_SEPARATOR") {
+      csv_sep <- arg_value
+    } else if (arg_name=="CSV_DECIMAL_SEP") {
+      csv_dec_sep <- arg_value
+    }
+  }
+
+  df <- read.csv2(csv_url, sep=csv_sep, dec='.')
+  return(df)
+}
+
+#' Gera uma string contendo os campos de uma tabela
+#'
+#' @param entity_metadata estrutura de dados com as colunas da entidade
+#' @return string contendo os campos separados por virgula
+extract_fields <- function(entity_metadata){
+  fields <- ""
+  for (i in 1:nrow(entity_metadata)) {
+    if (fields!=""){
+      fields <- paste(fields, ", ", sep="")
+    }
+    fields <- paste(fields, entity_metadata[i,2], sep="")
+  }
+  return(fields)
+}
--- R/load_data.R
+++ R/load_data.R
@@ -5,106 +5,20 @@
 #' apropriada.
 #'
 #' @param entity_catalog id of data catalog entity
-#' @return A matrix of the entity_catalog
+#' @return dados coletada da base fonte segundo o catalogo
 #' @export
 load_data <- function(entity_catalog){
-  path <- paste("https://raw.githubusercontent.com/suporteunisoma/databox-r/c943edd1ad740d9199fd0a10dbbcb2a2b7038b9b/", entity_catalog, ".csv", sep="")
-  df <- read.csv2(path, sep=",", dec=".")
-  df
+  source("R/extractor.R", encoding="UTF8")
 
-  entity_catalog = "bi_ginfra_helpdesk_ticket"
-  result <- load_meta_data(entity_catalog)
-  source <- result$entity_args[1,3]
+  md <- load_meta_data(entity_catalog)
+  source <- md$entity_args[1,3]
+
   #para cada tipo de 'source' acessa os dados de diferentes maneiras
   if (source=="JDBC_SOURCE") {
-    load_jdbc_data(result$entity_args, result$entity_definition, entity_catalog)
+    result = load_jdbc_data(md$entity_args, md$entity_definition, entity_catalog)
+  } else if (source=="CSV_URL_SOURCE") {
+    result = load_csv_url_data(md$entity_args, md$entity_definition, entity_catalog)
   }
 
-}
-
-
-load_meta_data <- function(entity_catalog) {
-  library(DBI)
-  library(rJava)
-  library(RJDBC)
-  library(tidyverse)
-
-  dbx_driver <- Sys.getenv("DATA_BOX_JDBC_DRIVER")
-  dbx_driver_path <- Sys.getenv("DATA_BOX_JDBC_LIB_PATH")
-  dbx_driver_host <- Sys.getenv("DATA_BOX_JDBC_HOST")
-  dbx_driver_user <- Sys.getenv("DATA_BOX_JDBC_USER")
-  dbx_driver_pwd <- Sys.getenv("DATA_BOX_JDBC_PWD")
-
-  if (dbx_driver=="") {
-    dbx_driver = "org.sqlite.JDBC"
-    dbx_driver_path = "/home/sqlite-jdbc-3.34.0.jar"
-    dbx_driver_host = "jdbc:sqlite:/home/data_box.db"
-    dbx_driver_user = "unisoma"
-    dbx_driver_pwd = ""
-  }
-  drv <- RJDBC::JDBC(dbx_driver, dbx_driver_path)
-  conn <- RJDBC::dbConnect(drv, dbx_driver_host, dbx_driver_user, dbx_driver_pwd)
-
-  # obtem os metadados para acesso ao dado requisitado
-  sql_args <- "select ea.arg_name, ea.arg_value, s.name as source
-               from dbx_source_engine_arg ea
-        inner join dbx_source_engine se on (ea.source_engine_id = se.source_id)
-                       inner join dbx_catalog c on (c.source_engine_id = se.id)
-                               inner join dbx_source s on (s.id = se.source_id)
-              where c.entity=?"
-  eargs <- dbGetQuery(conn, sql_args, entity_catalog)
-
-  # obtem os metadados para acesso ao dado requisitado
-  sql_defin <- "select cc.id, cc.name, cc.col_type, cc.is_not_null
-                from dbx_catalog c
-                inner join dbx_catalog_column cc on (cc.catalog_id = c.id)
-                where c.entity=?"
-  edefn <- dbGetQuery(conn, sql_defin, entity_catalog)
-
-  # retorna dois objetos: (a) os argumentos para se conectar na fonte dos dados
-  # e (b) a definicao dos metadados da entidade.
-  return(list(entity_args = eargs, entity_definition = edefn))
-
-}
-
-load_jdbc_data <- function(jdbc_args, entity_metadata, entity_catalog){
-
-  jdbc_driver = ""
-  jdbc_host = ""
-  jdbc_user = ""
-  jdbc_pwd = ""
-  jdbc_driver_path = ""
-
-  for (i in 1:nrow(results)) {
-    arg_name <- results[i,1]
-    arg_value <- results[i,2]
-
-    if (arg_name=="JDBC_DRIVER") {
-      jdbc_driver <- arg_value
-
-      #if (str_locate(jdbc_driver, "sqlite")[1,1]>0) {
-      #   jdbc_driver_path = "/home/sqlite-jdbc-3.34.0.jar"
-      #} else if (str_locate(jdbc_driver, "postgresql")[1,1]>0) {
-         jdbc_driver_path = "/home/postgresql.jar"
-      #}
-
-    } else if (arg_name=="JDBC_HOST") {
-      jdbc_host <- arg_value
-    } else if (arg_name=="JDBC_PWD") {
-      jdbc_pwd <- arg_value
-    } else if (arg_name=="JDBC_USER") {
-      jdbc_user <- arg_value
-    }
-  }
-
-  print(jdbc_driver)
-  print(jdbc_driver_path)
-  print(jdbc_host)
-  print(jdbc_user)
-  print(jdbc_pwd)
-
-  drv <- RJDBC::JDBC(jdbc_driver, jdbc_driver_path)
-  conn <- RJDBC::dbConnect(drv, jdbc_host, jdbc_user, jdbc_pwd)
-
-  print(conn)
+  return(result)
 }
