836503b277f581a559d3f3d80d2cb0541c7402ac
Suporte UniSoma <81772055+suporteunisoma@users.noreply.github.com>
2023-10-02 21:25:11 -0300

    ajustes coleta de dados do databox

--- R/load_data.R
+++ R/load_data.R
@@ -21,16 +21,20 @@ load_data <- function(entity_catalog){
   access_token <- dbx_authenticate()
   meta_data_sql <- dbx_call_service(access_token, entity_catalog)
 
-  #requisita do DataBOX os dados referentes ao Catalogo
-  print("Collecting data...")
-  setwd("~/.local/")
-  system(paste("/bin/python3", "/opt/trino/load_data.py", paste("'",meta_data_sql, "'", sep=''), sep=' '),
-      intern = TRUE, ignore.stdout = FALSE, ignore.stderr = FALSE
-  )
+  if (!is.null(meta_data_sql)) {
 
-  print("Convert RDS to Dataset...")
-  #retorna dados a partir de RDS volatil
-  result <- readRDS(dumpFile)
+     #requisita do DataBOX os dados referentes ao Catalogo
+     print("Collecting data...")
+     setwd("~/.local/")
+     system(paste("/bin/python3", "/opt/trino/load_data.py", paste("'",meta_data_sql, "'", sep=''), sep=' '),
+         intern = TRUE, ignore.stdout = FALSE, ignore.stderr = FALSE
+     )
+     print("Convert RDS to Dataset...")
+     #retorna dados a partir de RDS volatil
+     result <- readRDS(dumpFile)
+  } else {
+    print("A consulta SQL nao foi encontrada ou o catalogo nao existe.")
+  }
 
   return(result)
 }
@@ -42,9 +46,14 @@ dbx_call_service <- function(access_token, entity_catalog) {
   library(httr)
   library(jsonlite)
 
+  urlMetaData <- Sys.getenv("DATABOX_METADATA_URL")
+  if (urlMetaData=="") {
+    urlMetaData <- "http://192.168.7.221:9091/databox/api/dbxmetadata/findMetaDataByCatalogName/"
+  }
+
   print("Call Dbx service...")
   auth <- paste("Bearer", access_token, sep=' ')
-  res <- POST(paste("http://192.168.7.221:9091/databox/api/dbxmetadata/findMetaDataByCatalogName/", entity_catalog, sep=''),
+  res <- POST(paste(urlMetaData, entity_catalog, sep=''),
               add_headers(Authorization = auth, "accept"="application/json", "content-type"="application/json"))
   if (res$status_code==200) {
     json_content <- rawToChar(res$content)
@@ -64,15 +73,32 @@ dbx_authenticate <- function() {
   library(jsonlite)
   access_token <- ""
 
+  urlAuth <- Sys.getenv("DATABOX_ACCOUNTS_URL")
+  if (urlAuth=="") {
+    urlAuth <- "http://192.168.7.221:9092/authorization/oauth/token"
+  }
+  auth <- Sys.getenv("DATABOX_BASIC_AUTH")
+  if (auth=="") {
+    auth <- "SUQtQzIzMjMzQUEtQUI2Ri00REI0LUE2NEQtQjVFRDI0Nzk2NDJBOiRjZDY3OWYyMDczNGM0NzUxOGQ1NTQ1MTgwNjNlMTRkZg=="
+  }
+  usrAuth <- Sys.getenv("DATABOX_BASIC_AUTH_USER")
+  if (usrAuth=="") {
+    usrAuth <- "unisoma"
+  }
+  usrPass <- Sys.getenv("DATABOX_BASIC_AUTH_PASS")
+  if (usrPass=="") {
+    usrPass <- "unisoma"
+  }
+
   print("Dbx Authentication...")
   login <- list(
     grant_type = "password",
-    username = "unisoma",
-    password = "unisoma"
+    username = usrAuth,
+    password = usrPass
   )
-  auth <- "Basic SUQtQzIzMjMzQUEtQUI2Ri00REI0LUE2NEQtQjVFRDI0Nzk2NDJBOiRjZDY3OWYyMDczNGM0NzUxOGQ1NTQ1MTgwNjNlMTRkZg=="
-  res <- POST("http://192.168.7.221:9092/authorization/oauth/token",
-              add_headers(Authorization = auth ),
+
+  res <- POST(urlAuth,
+              add_headers(Authorization = paste('Basic', auth, sep=' ') ),
               body = login)
   if (res$status_code==200) {
     json_content <- rawToChar(res$content)
