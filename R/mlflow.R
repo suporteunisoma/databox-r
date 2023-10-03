7498d2efb42ce25f2c53914844ecaac29f519bf8
Suporte UniSoma <81772055+suporteunisoma@users.noreply.github.com>
2023-10-02 21:26:09 -0300

    ajustes na rotina para integração com MLFLow

--- R/mlflow.R
+++ R/mlflow.R
@@ -11,26 +11,23 @@ start_mlflow <- function(experiment_name){
   library(mlflow)
 
   # conecta no servidor MLFlow e retorna um objeto client MLFlow
-  mlflow_track_uri <- Sys.getenv("MLFLOW_TRACKING_URI")
-  if (mlflow_track_uri=="") {
-    mlflow_track_uri = "http://192.168.7.234:5000/"
-  }
-  client_obj <- mlflow_client(tracking_uri = mlflow_track_uri)
+  mlflow_track_uri <- get_MlFlow_Url()
+  mlflow::mlflow_set_tracking_uri(mlflow_track_uri)
+  client_obj <- mlflow::mlflow_client()
+  print(mlflow::mlflow_get_tracking_uri())
+
 
   # coleta os experimentos do MLFlow e verifica se o nome do experimento
   # enviado por parametro já existe.
-  exp_id <- checkExperiment(experiment_name = experiment_name, client = client_obj)
+  exp_id <- checkExperiment(experiment_name = experiment_name,
+                            mlflow_client = client_obj)
 
-  # se o experimento ainda nao existir, cria um novo registro
-  if (exp_id==0) {
-    exp_id = mlflow_create_experiment(name=experiment_name, client = client_obj)
-  }
 
   # cria uma nova rodada
-  run_obj = mlflow_start_run(
-    experiment_id = exp_id,
-    client = client_obj
-  )
+  id = as.integer(exp_id[1,1])
+  with(run_obj <-mlflow_start_run(experiment_id = id), {
+    print("run Forest!!!")
+  })
 
   return (run_obj);
 }
@@ -49,12 +46,6 @@ save_artifact <- function(run_obj, df_artifact=NULL, file_path_artifact=NULL){
   library(mlflow)
   library(readr)
   library(uuid)
-
-  # define URI do servidor MLFlow
-  mlflow_track_uri <- Sys.getenv("MLFLOW_TRACKING_URI")
-  if (mlflow_track_uri=="") {
-    Sys.setenv(MLFLOW_TRACKING_URI = "http://192.168.7.234:5000/")
-  }
 
   if (missing(file_path_artifact)==FALSE)  {
     system("mlflow --version")
@@ -92,12 +83,11 @@ finish_mlflow <- function(run_obj, df_parameter=NULL, df_metric=NULL, final_stat
 
   library(mlflow)
 
-  # conecta no servidor MLFlow e retorna um objeto client MLFlow
-  mlflow_track_uri <- Sys.getenv("MLFLOW_TRACKING_URI")
-  if (mlflow_track_uri=="") {
-    mlflow_track_uri = "http://192.168.7.234:5000/"
-  }
-  client_obj <- mlflow_client(tracking_uri = mlflow_track_uri)
+  # retona objeto contendo credenciais do MLFlow
+  mlflow_track_uri <- get_MlFlow_Url()
+  mlflow::mlflow_set_tracking_uri(mlflow_track_uri)
+  client_obj <- mlflow::mlflow_client()
+  print(mlflow::mlflow_get_tracking_uri())
 
   # salva a lista de parametros do modelo
   if (missing(df_parameter)==FALSE)  {
@@ -115,7 +105,7 @@ finish_mlflow <- function(run_obj, df_parameter=NULL, df_metric=NULL, final_stat
     }
   }
 
-  mlflow_end_run(
+  mlflow::mlflow_end_run(
     status = final_status,
     run_id = run_obj$run_uuid,
     client = client_obj
@@ -130,24 +120,23 @@ finish_mlflow <- function(run_obj, df_parameter=NULL, df_metric=NULL, final_stat
 #' @param experiment_name nome unico do experimento MLFlow
 #' @param client objeto contendo o MLFlow client
 #' @return id do experimento do MLFlow ou 0 caso nao exista
-checkExperiment <- function(experiment_name, client){
+checkExperiment <- function(experiment_name, mlflow_client){
 
-  //executa method da API R do MLFLOW para puxar os expercimentos disponiveis
-  exper_list <- mlflow_client_list_experiments(
-    client = client
-    view_type = c("ALL"),
+  exp_id <- NULL
+  try(
+      exp_id <- mlflow::mlflow_get_experiment(name=experiment_name)
   )
+  print(exp_id)
 
-  exp_id = 0
-  for(i in 1:nrow(exper_list)) {
-    row <- exper_list[i,]
-    if (row$name==experiment_name) {
-      exp_id = row$experiment_id
-
-      if (row$lifecycle_stage=="deleted") {
-        mlflow_restore_experiment(exp_id, client)
-      }
-
+  if (is.null(exp_id)) {
+      expId <- mlflow::mlflow_create_experiment(experiment_name,
+        client = mlflow_client
+      )
+      print(expId)
+  } else {
+    print("MLFlow experiment already exists..")
+    if (exp_id[1,4]=="deleted") {
+      print("The experiment exists but cannot be used because was already deleted")
     }
   }
 
@@ -165,4 +154,18 @@ checkExperiment <- function(experiment_name, client){
 #' @export
 MLFlowStatusEnum <- function() {
   list(FINISHED = "FINISHED", FAILED = "FAILED", KILLED = "KILLED")
+}
+
+
+
+#' Retorna a URL do MLFlow de acordo com a variavel de ambiente MLFLOW_URI
+#' Caso contrario retorna um valor padrao.
+#'
+#' @return string contendo a URL do MLFlow utilizada pela lib para integracao
+get_MlFlow_Url <- function(){
+  uri <- Sys.getenv("MLFLOW_URI")
+  if (uri=="") {
+    uri = "http://192.168.7.234:5000"
+  }
+  return(uri)
 }
